d4e2c347460dc7a7935b94e8d5c0e8fc8d5dcee6
Magr Cruz <maria.cruz@ucsp.edu.pe>
2023-04-09 13:06:19 -0500

    Added identation to code

--- Scanner.py
+++ Scanner.py
@@ -104,102 +104,103 @@ class Scanner():
             return None
 
     '''
-        def getToken(buffer_aux):
-  newline = True
-  buffer_aux = saltar_fin_cadena(buffer_aux)
-  char,buffer_aux = getchar(buffer_aux)
-# Identación
-#                 Algoritmo para identación:
-# si se tiene un salto de linea es porque se tendra una new line
-# solo es verdadero cuando se inicia y cuando se entra en saltar_fin_cadena
-  if char == " " and newline:
-    val = 1
-    char = getchar(buffer_aux)[0]
-    while char == " ":
-     val+=1
-     buffer_aux = getchar(buffer_aux)[1]
-     char = getchar(buffer_aux)[0]
-    print("ident",val)
-    newline = False
-    return buffer_aux
-#Algoritmo para los comentarios
-  if char == "/":
-    char = getchar(buffer_aux)[0]
-    if char == "/":
-      buffer_aux = goto("\n",buffer_aux)
-      buffer_aux = getToken(buffer_aux)
-      return buffer_aux
-    elif char == "*":
-      char, buffer_aux = getchar(buffer_aux)
-      buffer_aux = goto("*",buffer_aux)
-      char,buffer_aux = getchar(buffer_aux)
-      while char != "/" :
-        if len(buffer_aux)==0:
-          print("Comentario sin cerrar")
-          return buffer_aux
-        buffer_aux = goto("*",buffer_aux)
-        char,buffer_aux = getchar(buffer_aux)
-      buffer_aux = getToken(buffer_aux)
-      return buffer_aux
-    else:
-      print("Div")
-      return buffer_aux
-# Tokens
-  if char == "(":
-    print("lparen","(")
-  elif char == ")":
-    print("rparen",")")
-  elif char == "+":
-    print("rparen","+")
-  elif char == "-":
-    print("rparen","-")
-  elif char == "*":
-    print("rparen","*")
-  elif char == ":":
-    char = getchar(buffer_aux)[0]
-    if char == "=":
-      buffer_aux = getchar(buffer_aux)[1]
-      print("assing",":=")
-    else:
-      print("Falta \"=\" para la asignación","")
-  elif char == ".":
-    val = char
-    char = getchar(buffer_aux)[0]
-    if isNm(char):
-      while(isNm(char)):
-        buffer_aux = getchar(buffer_aux)[1]
-        val+=char
+def getToken(buffer_aux):
+    newline = True
+    buffer_aux = saltar_fin_cadena(buffer_aux)
+    char, buffer_aux = getchar(buffer_aux)
+    # Identación
+    #                 Algoritmo para identación:
+    # si se tiene un salto de linea es porque se tendra una new line
+    # solo es verdadero cuando se inicia y cuando se entra en saltar_fin_cadena
+    if char == " " and newline:
+        val = 1
         char = getchar(buffer_aux)[0]
-      print("digit",val)
-    else:
-      print("Falta un digito despues de \".\"","")
-  elif isNm(char):
-    val = char
-    char = getchar(buffer_aux)[0]
-    while(isNm(char)):
-      buffer_aux = getchar(buffer_aux)[1]
-      val+=char
-      char = getchar(buffer_aux)[0]
-    if char == '.':
-      val += char
-      buffer_aux = getchar(buffer_aux)[1]
-      char = getchar(buffer_aux)[0]
-    while(isNm(char)):
-      buffer_aux = getchar(buffer_aux)[1]
-      val+=char
-      char = getchar(buffer_aux)[0]
-    print("num",val)
-  elif isID(char):
-    val = char
-    char = getchar(buffer_aux)[0]
-    while(isID(char)or isNm(char)):
-      buffer_aux = getchar(buffer_aux)[1]
-      val+=char
-      char = getchar(buffer_aux)[0]
-    print("id",val)
+        while char == " ":
+            val += 1
+            buffer_aux = getchar(buffer_aux)[1]
+            char = getchar(buffer_aux)[0]
+        print("ident", val)
+        newline = False
+        return buffer_aux
+    # Algoritmo para los comentarios
+    if char == "/":
+        char = getchar(buffer_aux)[0]
+        if char == "/":
+            buffer_aux = goto("\n", buffer_aux)
+            buffer_aux = getToken(buffer_aux)
+            return buffer_aux
+        elif char == "*":
+            char, buffer_aux = getchar(buffer_aux)
+            buffer_aux = goto("*", buffer_aux)
+            char, buffer_aux = getchar(buffer_aux)
+            while char != "/":
+                if len(buffer_aux) == 0:
+                    print("Comentario sin cerrar")
+                    return buffer_aux
+                buffer_aux = goto("*", buffer_aux)
+                char, buffer_aux = getchar(buffer_aux)
+            buffer_aux = getToken(buffer_aux)
+            return buffer_aux
+        else:
+            print("Div")
+            return buffer_aux
+    # Tokens
+    if char == "(":
+        print("lparen", "(")
+    elif char == ")":
+        print("rparen", ")")
+    elif char == "+":
+        print("rparen", "+")
+    elif char == "-":
+        print("rparen", "-")
+    elif char == "*":
+        print("rparen", "*")
+    elif char == ":":
+        char = getchar(buffer_aux)[0]
+        if char == "=":
+            buffer_aux = getchar(buffer_aux)[1]
+            print("assing", ":=")
+        else:
+            print('Falta "=" para la asignación', "")
+    elif char == ".":
+        val = char
+        char = getchar(buffer_aux)[0]
+        if isNm(char):
+            while isNm(char):
+                buffer_aux = getchar(buffer_aux)[1]
+                val += char
+                char = getchar(buffer_aux)[0]
+            print("digit", val)
+        else:
+            print('Falta un digito despues de "."', "")
+    elif isNm(char):
+        val = char
+        char = getchar(buffer_aux)[0]
+        while isNm(char):
+            buffer_aux = getchar(buffer_aux)[1]
+            val += char
+            char = getchar(buffer_aux)[0]
+        if char == ".":
+            val += char
+            buffer_aux = getchar(buffer_aux)[1]
+            char = getchar(buffer_aux)[0]
+        while isNm(char):
+            buffer_aux = getchar(buffer_aux)[1]
+            val += char
+            char = getchar(buffer_aux)[0]
+        print("num", val)
+    elif isID(char):
+        val = char
+        char = getchar(buffer_aux)[0]
+        while isID(char) or isNm(char):
+            buffer_aux = getchar(buffer_aux)[1]
+            val += char
+            char = getchar(buffer_aux)[0]
+        print("id", val)
 
-  return buffer_aux
+    return buffer_aux
 '''
+
     def getToken(self): #(tokentype, tokenval)
         ##      skip_space()
         ##      getchar()
