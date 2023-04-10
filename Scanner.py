88e70bf169c12c10ce23858cfd6085aca028c808
Rubén Félix Guzmán Chiroque <54367628+RFGCH@users.noreply.github.com>
2023-04-09 21:26:32 -0500

    Avanze de la primera parte 1
    Se a agregado:
    - Token numero
    - Lectura de buffer
    - Lectura de tokens hasta que se vacié el buffer
    - Mitad de comentarios

--- Scanner.py
+++ Scanner.py
@@ -39,11 +39,11 @@ class Scanner():
         self.tokens = []
         self.errores = []
 
-        self.pos = [0,0] # Revisar, se podria usar alguna estructura
-        self.current_char = None
+        self.pos = [1,0] # Revisar, se podria usar alguna estructura
+        self.current_char = ""
         self.lexeme = ""
         self.state = 0
-        self.newLine = True
+        self.newLine = False
 
         #Aux
         self.firstOfDblOper = []
@@ -54,6 +54,7 @@ class Scanner():
 
     def begin(self, file):
         archivo = open(file)
+        self.buffer = self.archivo.read()
         self.errores = []
     
     def getFirstOfDblOper(self):
@@ -63,17 +64,6 @@ class Scanner():
     ### FUNCIONES
     def getInteger(self):
         print("getInteger")
-        
-    ##      def isLet(self):
-        ##      if LETTER.find(current_char) != -1:
-            ##      return True
-        ##      return False
-    ##      def isNum(self):
-        ##      if NUMBER.find(current_char) != -1:
-            ##      return True
-        ##      return False        
-
-    ####
 
     def getchar(self): # Se actualiza current_char
         # Se puede trabajar como tipo de char
@@ -83,29 +73,27 @@ class Scanner():
         else:
             self.pos[1] += 1
             # Obtiene el primer caracter y lo pone en self current char
-            self.buffer.lstrip(self.current_char)
+            self.current_char = self.buffer[0]
+            self.buffer = self.buffer[1:]
 
     def peekchar(self):
         if len(self.buffer == 0):
             return ""
         return self.buffer[0]
-
-# si se hace por lineas
-    ##      def skip_space(self):
-        ##      if len(buffer) == 0:    return
-        ##      while buffer[0] == ' ':
-            ##      current_posx += 1
-            ##      buffer = buffer[1:]
-        ##      if len(buffer) != 0 and buffer[0] == '\n':   
-            ##      newline = True
-            ##      current_line += 1
-            ##      buffer = buffer[1:]
     
     def ignoreSpaces(self):
         if self.current_char == " ":
             while self.peekchar() == " ":
                 self.getchar()
-
+                
+    def ignoreComents(self):
+        c = self.pos[1]
+        if self.current_char == "#":
+            while self.current_char != '\n':
+                self.getchar()
+            self.pos[1] = c
+        if c == 1:
+            
     def getIdentifier(self):
         # Consume todos los caracteres dentro de la expresion [a-z|A-Z][a-z|A-Z|0-9|_]*
         self.lexeme = self.current_char
@@ -114,7 +102,30 @@ class Scanner():
             self.getchar()
             self.lexeme += self.current_char
             c = self.peekchar()
-
+    def getNumber(self):
+        # Consume todos los caracteres dentro de la expresion [a-z|A-Z][a-z|A-Z|0-9|_]*
+        self.lexeme = self.current_char
+        c = self.peekchar()
+        while c.isdigit():
+            self.getchar()
+            self.lexeme += self.current_char
+            c = self.peekchar()
+        if len(self.lexeme) > 1 and self.lexeme[0] == '0': 
+          print("Numero invalido: El numero no puede tener un 0 a la izquierda")
+          return False
+        if len(self.lexeme) == 10 :
+          if int(self.lexeme[:9]) == 214748364:
+            if int(self.lexeme[9]) > 7:
+              print("Numero invalido: EL numero es muy grande")
+              return False
+          elif int(self.lexeme[:9]) > 214748364:
+            print("Numero invalido: EL numero es muy grande")
+            return False
+        elif len(self.lexeme) > 10:
+          print("Numero invalido: EL numero es muy grande")
+          return False
+        return True
+    
     def getTknOper(self):
         self.lexeme = self.current_char
         if self.current_char in self.firstOfDblOper:
@@ -126,122 +137,30 @@ class Scanner():
                 return Token(OPER_DELIMITERS_2[oper], oper, self.pos[0], self.pos[1])
         return Token(OPER_DELIMITERS[self.lexeme], self.lexeme, self.pos[0], self.pos[1])
 
-    '''
-def getToken(buffer_aux):
-    newline = True
-    buffer_aux = saltar_fin_cadena(buffer_aux)
-    char, buffer_aux = getchar(buffer_aux)
-    # Identación
-    #                 Algoritmo para identación:
-    # si se tiene un salto de linea es porque se tendra una new line
-    # solo es verdadero cuando se inicia y cuando se entra en saltar_fin_cadena
-    if char == " " and newline:
-        val = 1
-        char = getchar(buffer_aux)[0]
-        while char == " ":
-            val += 1
-            buffer_aux = getchar(buffer_aux)[1]
-            char = getchar(buffer_aux)[0]
-        print("ident", val)
-        newline = False
-        return buffer_aux
-    # Algoritmo para los comentarios
-    if char == "/":
-        char = getchar(buffer_aux)[0]
-        if char == "/":
-            buffer_aux = goto("\n", buffer_aux)
-            buffer_aux = getToken(buffer_aux)
-            return buffer_aux
-        elif char == "*":
-            char, buffer_aux = getchar(buffer_aux)
-            buffer_aux = goto("*", buffer_aux)
-            char, buffer_aux = getchar(buffer_aux)
-            while char != "/":
-                if len(buffer_aux) == 0:
-                    print("Comentario sin cerrar")
-                    return buffer_aux
-                buffer_aux = goto("*", buffer_aux)
-                char, buffer_aux = getchar(buffer_aux)
-            buffer_aux = getToken(buffer_aux)
-            return buffer_aux
-        else:
-            print("Div")
-            return buffer_aux
-    # Tokens
-    if char == "(":
-        print("lparen", "(")
-    elif char == ")":
-        print("rparen", ")")
-    elif char == "+":
-        print("rparen", "+")
-    elif char == "-":
-        print("rparen", "-")
-    elif char == "*":
-        print("rparen", "*")
-    elif char == ":":
-        char = getchar(buffer_aux)[0]
-        if char == "=":
-            buffer_aux = getchar(buffer_aux)[1]
-            print("assing", ":=")
-        else:
-            print('Falta "=" para la asignación', "")
-    elif char == ".":
-        val = char
-        char = getchar(buffer_aux)[0]
-        if isNm(char):
-            while isNm(char):
-                buffer_aux = getchar(buffer_aux)[1]
-                val += char
-                char = getchar(buffer_aux)[0]
-            print("digit", val)
-        else:
-            print('Falta un digito despues de "."', "")
-    elif isNm(char):
-        val = char
-        char = getchar(buffer_aux)[0]
-        while isNm(char):
-            buffer_aux = getchar(buffer_aux)[1]
-            val += char
-            char = getchar(buffer_aux)[0]
-        if char == ".":
-            val += char
-            buffer_aux = getchar(buffer_aux)[1]
-            char = getchar(buffer_aux)[0]
-        while isNm(char):
-            buffer_aux = getchar(buffer_aux)[1]
-            val += char
-            char = getchar(buffer_aux)[0]
-        print("num", val)
-    elif isID(char):
-        val = char
-        char = getchar(buffer_aux)[0]
-        while isID(char) or isNm(char):
-            buffer_aux = getchar(buffer_aux)[1]
-            val += char
-            char = getchar(buffer_aux)[0]
-        print("id", val)
-
-    return buffer_aux
-'''
-
     def getToken(self): #(tokentype, tokenval)
         self.lexeme = ""
         token = None
         self.getchar() #Nota, no consumir chars de otros tokens, en cambio usar peek char
-
-        # Buscar salto de linea
-        # Tener cuidado con logica para identificar lineas vacias
-        if self.current_char == "\n":
-            print("Tkn NEWLINE")
-            self.newLine = True
-            return
         
-        if self.newLine:
-            print("INDENT and DEDENT")
-            return "token de ident o dedent"
-        
+        # Limpiamos espacios vacios y comentarios
         if not self.newLine: 
             self.ignoreSpaces()
+        self.ignoreComents()
+        # Si hay un 
+        if self.current_char == '\n':
+            self.newLine = True
+            aux_x = self.pos[0]
+            aux_y = self.pos[1]
+            self.pos[0] += 1
+            self.pos[1] = 0
+            return Token("NEWLINE","", aux_x, aux_y)
+        # 
+        if self.newLine:
+            if self.current_char != " ":
+              self.newLine = False
+            else:
+              return Token("IDENT","", self.pos[0], self.pos[1])
+
         # Esta buscando un id o una palabra reservada
         if self.current_char.isalpha():
             self.getIdentifier()
@@ -252,7 +171,9 @@ def getToken(buffer_aux):
 
         # Esta buscando un numero
         elif self.current_char.isdigit():
-            print("Digit")
+            if self.getNumber():
+              token = Token("NUMBER",self.lexeme,self.pos[0], self.pos[1])
+            else: print("Error")
         
         # Buscar operadores
         elif self.current_char in OPER_DELIMITERS or self.current_char in self.firstOfDblOper:
@@ -268,14 +189,13 @@ def getToken(buffer_aux):
     def debug(self):
         print("INFO SCAN - Start scanning...")
         # Falta ver los errores
-        while True: # For now
+        while len(self.buffer) != 0: # For now
             token = self.getToken()
             #if token == ERROR: self.errores.append(token)
 
             self.tokens.append(token)
             print("DEBUG SCAN - %s " % (token))
-            if token == EOF:
-                break
+
 
 s = Scanner()
 s.begin("file.txt")
